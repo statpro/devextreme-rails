ccf393d79fd3a59aebb9839e0eb2ce1217a7f48f
ChristopherRollins <christopher.rollins@live.com>
2019-05-29 10:17:32 +0200

    * fixed browser compatibility

--- app/assets/javascripts/data_table.js
+++ app/assets/javascripts/data_table.js
@@ -1,11 +1,11 @@
 function ItemResize(dataGrid, height, width) {
   if (!(height || width)){
-    let screenHeight = $('.navbar-fixed-bottom').offset().top - $('#' + dataGrid.attr('id') + '-holder').offset().top;
-    let footerHeight = $('footer').height();
+    var screenHeight = $('.navbar-fixed-bottom').offset().top - $('#' + dataGrid.attr('id') + '-holder').offset().top;
+    var footerHeight = $('footer').height();
     window.setGridHolderHeight = screenHeight - footerHeight;
     dataGrid.height( screenHeight - footerHeight );
   } else {
-    let dgMargin = dataGrid.outerHeight(true)-dataGrid.outerHeight();
+    var dgMargin = dataGrid.outerHeight(true)-dataGrid.outerHeight();
     window.setGridHolderHeight = parseInt(height)-dgMargin;
     dataGrid.height( parseInt(height)-dgMargin );
   }
