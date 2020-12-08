40771e1c312dc12ebbc1f3fc74ce96babbd3683a
Christopher Rollins <christopher.rollins@live.com>
2020-12-08 10:50:38 +0200

    DX-053 | Add bulk action helper

--- app/assets/javascripts/data_table.js
+++ app/assets/javascripts/data_table.js
@@ -24,6 +24,51 @@ function getSelectedRowKeys(container_id) {
   return dataGrid.getSelectedRowKeys();
 }
 
+function getSelectedRowsAsParams(container_id, filter_form_id = '') {
+  var keys = getSelectedRowKeys("#" + container_id);
+
+  var params = {};
+
+  if (keys.length > 0) {
+    params = {
+      ids: keys,
+      container_id: container_id
+    };
+  }
+  else {
+    // FIXME: this isn't a specific enough jQuery selector when filter_form_id == "form"
+    //   since if there is more than one form, it will include them all
+    var search_params = $("" + filter_form_id + ".auto-submit").serializeArray();
+    var data = {
+      name: 'container_id',
+      value: container_id
+    };
+    search_params.push(data);
+    params = search_params;
+  }
+
+  return params;
+}
+
+function submit_bulk_action(caller, container_id, fn_callback, confirm_message_selector) {
+  var $caller = $(caller);
+  var dataGrid = getDataGrid('#' + container_id);
+  var keys = dataGrid.getSelectedRowKeys();
+  var value_count = keys.length > 0 ? keys.length : dataGrid.totalCount();
+
+  if (fn_callback) {
+    fn_callback(keys);
+  }
+
+  var confirmation_message = $caller.data('confirmMessage').replace('{0}', value_count);
+  if (confirm_message_selector) {
+    $(confirm_message_selector).text(confirmation_message);
+  }
+  else {
+    $caller.data('confirm', confirmation_message);
+  }
+}
+
 function download_file(event, type, container_id, allow_large_files) {
   container_id = getSelectedContainerId(container_id);
 
