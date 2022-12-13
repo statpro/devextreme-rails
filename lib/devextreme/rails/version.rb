2f528a335018bdaef141644e87b70065f41277a8
Christopher Rollins <christopher.rollins@live.com>
2022-12-13 14:06:32 +0200

    bump version

--- lib/devextreme/rails/version.rb
+++ lib/devextreme/rails/version.rb
@@ -2,6 +2,6 @@ module Devextreme
   module Rails
     # use the same version number as the dx.webappjs
     # with the extra minor version to represent any version changes to this wrapper gem but not the underlying devextreme js.
-    VERSION = "22.1.3.0.6"
+    VERSION = "22.1.6-0.1"
   end
 end
