dd40f282ccc4a5af7332958f0224983cfe5a27ef
Gabriel Strauss <gstraus@confluence.com>
2024-07-03 11:30:43 +0200

    chore: bump version

--- lib/devextreme/rails/version.rb
+++ lib/devextreme/rails/version.rb
@@ -2,6 +2,6 @@ module Devextreme
   module Rails
     # use the same version number as the dx.webappjs
     # with the extra minor version to represent any version changes to this wrapper gem but not the underlying devextreme js.
-    VERSION = "22.1.6-0.28"
+    VERSION = "22.1.6-0.29"
   end
 end
